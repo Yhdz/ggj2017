4012622b80f5c16bbfb673be4567e3ca45dfa9dc
Ilja Plutschouw <ilja.plutschouw@gmail.com>
2017-01-21 12:49:56 +0100

    Boat + boat spawner added

--- Assets/Boat.cs
+++ Assets/Boat.cs
@@ -3,22 +3,31 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Boat : MonoBehaviour {
+	public float speed;
+	public int party;
+
 	private StringSimulation sea;
 	private Rigidbody2D rigidBody;
 	private float randomTimeOffset;
+	private int state = 0;			// 0: floating, 1: sinking
 
 	// Use this for initialization
 	void Start () {
 		sea = FindObjectOfType<StringSimulation> ();
 		rigidBody = GetComponent<Rigidbody2D> ();
-		randomTimeOffset = Random.Range (0.0f, Mathf.PI * 2);
+		randomTimeOffset = Random.Range (0.0f, Mathf.PI * 2.0f);
 	}
 
-	void FixedUpdateFloatPhysics()
+	public void SetColor(Color color)
 	{
-		Vector2 heightVelocity = sea.GetHeightVelocity (transform.position.x);
+		GetComponent<SpriteRenderer> ().color = color;
+	}
 
-		float heightDiff = heightVelocity.x - transform.position.y;
+	void FixedUpdateFloatPhysics(float waterHeight, float waterAngle)
+	{
+		Vector3 heightVelocityAngle = sea.GetHeightVelocity (transform.position.x);
+
+		float heightDiff = heightVelocityAngle.x - transform.position.y;
 		rigidBody.drag = 0.1f;
 		if (heightDiff > 0.0f) {
 			float force = heightDiff * 100.0f;
@@ -30,19 +39,42 @@ public class Boat : MonoBehaviour {
 		}
 	}
 
-	void FixedUpdateStickToWater()
+	void FixedUpdateStickToWater(float waterHeight, float waterAngle)
 	{
-		Vector2 heightVelocity = sea.GetHeightVelocity (transform.position.x);
 		float slowBounce = Mathf.Sin (Time.time * 8.0f + randomTimeOffset) * 0.02f;
 
-		transform.position = new Vector3 (transform.position.x, heightVelocity.x + slowBounce, transform.position.z);
-		transform.rotation = Quaternion.Euler(0.0f, 0.0f, Mathf.Sin(Time.time * 3.0f + randomTimeOffset) * 5.0f);
+		transform.position = new Vector3 (transform.position.x, waterHeight + slowBounce, transform.position.z);
+		transform.rotation = Quaternion.Euler(0.0f, 0.0f, Mathf.Sin(Time.time * 3.0f + randomTimeOffset) * 3.0f + waterAngle);
 	}
 
 	void FixedUpdate()
 	{
-		//FixedUpdateFloatPhysics();
-		FixedUpdateStickToWater();
+		if (state == 0) {
+			Vector3 heightVelocityAngle = sea.GetHeightVelocity (transform.position.x);
+
+			// Update bouncing on water
+			//FixedUpdateFloatPhysics();
+			FixedUpdateStickToWater (heightVelocityAngle.x, heightVelocityAngle.z);
+
+			// update x position
+			transform.Translate (new Vector3 (speed * Time.fixedDeltaTime, 0.0f, 0.0f));
+			if (Mathf.Abs (transform.position.x) > 7.0f) {
+				speed *= -1.0f;
+			}
+
+			if (Mathf.Abs (heightVelocityAngle.y) > 0.01f) {
+				Debug.Log ("Sinking");
+				state = 1;
+			}
+
+		} else if (state == 1) {	// sinking ship
+			transform.Translate (new Vector3 (0.0f, -0.5f * Time.fixedDeltaTime, 0.0f), Space.World);
+			if (transform.position.y < -5.0f) {
+				state = 2;
+			}
+		} else if (state == 2) {
+			// sunken ship
+		}
 	}
 	
 	// Update is called once per frame
--- Assets/BoatSpawner.cs
+++ Assets/BoatSpawner.cs
@@ -7,7 +7,21 @@ public class BoatSpawner : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
+		// spawn boats for every player
+		for (int player = 0; player < 2; player++) {
+			for (int boatNumber = 0; boatNumber < 10; boatNumber++) {
+				float randomPosition = Random.Range (-7.0f, 7.0f);
+				Boat boat = Instantiate (BoatPrefab, new Vector3 (randomPosition, 0.0f, 0.0f), Quaternion.identity).GetComponent<Boat> ();;
+				boat.party = player;
+				boat.speed = (Random.Range (0, 2) * 2 - 1) * boat.speed;
+				if (boat.party == 0) {
+					boat.SetColor (Color.red);
+				} else {
+					boat.SetColor (Color.blue);
+				}
+			}
+		}
+
 	}
 	
 	// Update is called once per frame
