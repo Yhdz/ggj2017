e8632857a781f62d8e88065f9cadead35f4e9403
Ilja Plutschouw <ilja.plutschouw@gmail.com>
2017-01-21 17:02:57 +0100

    explosion changed - limited max speed

--- Assets/Scripts/Kraken.cs
+++ Assets/Scripts/Kraken.cs
@@ -69,7 +69,7 @@ public class Kraken : MonoBehaviour {
 		Vector2 deltaForward = new Vector2 ();
 		if (swim > 0) {
 			momentum = swim;
-			speed += (maxSpeed - minSpeed) / .1f * Time.deltaTime;
+			speed += (maxSpeed - speed) / .1f * Time.deltaTime;
 			deltaForward = momentum * speed * Vector2.up * Time.deltaTime * Mathf.Abs (Mathf.Sin (strokePhase));
 			strokePhase += strokeFrequency * Time.deltaTime;
 
@@ -107,18 +107,23 @@ public class Kraken : MonoBehaviour {
 		if (playerPressurePanel != null) {
 			float depth = sea.GetHeightVelocity (transform.position.x).x - transform.position.y;
 			if (depth < 2.0f) {
-				pressure += 0.4f * Time.deltaTime;
+				pressure += 0.2f * Time.deltaTime;
 			} else {
-				pressure -= 0.2f * Time.deltaTime;
+				pressure -= 0.1f * Time.deltaTime;
 			}
 
 			if (pressure >= 1.0f) {
 				// explode!!
+				Destroy (this.gameObject);
 				Instantiate(explosionParticleSystem, transform.position, transform.rotation);
-				Destroy (this);
 			}
 
 			pressure = Mathf.Clamp01 (pressure);
+			Color playerColor = Color.red;
+			if (playerNumber == 1) {
+				playerColor = Color.blue;
+			}
+				
 			playerPressurePanel.color = Color.Lerp (Color.white, Color.red, pressure);
 
 		}
